b320a62a8ba953be6a1a2412d71a2ecde7b43c9a
Salino3 <flaviobcn012@gmail.com>
2024-06-15 10:52:34 +0200

    fix width img

--- src/pods/home/components/image-rotate/image-rotate.component.tsx
+++ src/pods/home/components/image-rotate/image-rotate.component.tsx
@@ -2,19 +2,39 @@ import React, { useState } from "react";
 import "./rotate-image.styles.scss";
 
 export const RotateImage: React.FC = () => {
-  const [rotateImg, setRotateImg] = useState(90);
+  const [rotateImg, setRotateImg] = useState<number>(90);
+  const [heightImg, setHeightImg] = useState<number | null>(null);
 
   const handleRotate = () => {
-    setRotateImg(rotateImg + 90);
+    if (rotateImg >= 360) {
+      setRotateImg(90);
+      // setHeightImg(null);
+      // return;
+    } else {
+      setRotateImg(rotateImg + 90);
+    }
+    const container: HTMLElement | null = document.getElementById("container");
     const img: HTMLImageElement | null = document.querySelector("img");
     if (img) {
       img.style.transform = `rotate(${rotateImg}deg)`;
+    }
+    if (container && img) {
+      container.style.transform = `rotate(- ${rotateImg}deg)`;
+      //   container.style.height = `${img.clientHeight}px`;
+      setHeightImg(img.clientHeight);
     }
   };
 
   return (
     <div className="rootRotateImage">
-      <div className="container">
+      <div
+        style={{
+          height: rotateImg == 180 || rotateImg == 360 ? `${400}px` : ``,
+          width: rotateImg == 180 || rotateImg == 360 ? `${heightImg}px` : "",
+        }}
+        id="container"
+        className="container"
+      >
         <div className="containerIcon ">
           <i onClick={handleRotate} className="fas fa-sync-alt iconImg"></i>
         </div>
