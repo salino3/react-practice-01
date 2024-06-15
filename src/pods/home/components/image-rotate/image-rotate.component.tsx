b1525e96f9b03e75647dcc5d56f37a77df1fc6ac
Salino3 <flaviobcn012@gmail.com>
2024-06-15 11:44:52 +0200

    add navigator.userAgent

--- src/pods/home/components/image-rotate/image-rotate.component.tsx
+++ src/pods/home/components/image-rotate/image-rotate.component.tsx
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./rotate-image.styles.scss";
 
 export const RotateImage: React.FC = () => {
   const [rotateImg, setRotateImg] = useState<number>(90);
   const [heightImg, setHeightImg] = useState<number | null>(null);
+  const [widthtImg, setWidthtImg] = useState<number | null>(null);
+  const [instructionText, setInstructionText] = useState<string>(
+    "(hover over the image)"
+  );
 
   const handleRotate = () => {
     if (rotateImg >= 360) {
       setRotateImg(90);
-      // setHeightImg(null);
-      // return;
     } else {
       setRotateImg(rotateImg + 90);
     }
@@ -20,16 +22,38 @@ export const RotateImage: React.FC = () => {
     }
     if (container && img) {
       container.style.transform = `rotate(- ${rotateImg}deg)`;
-      //   container.style.height = `${img.clientHeight}px`;
       setHeightImg(img.clientHeight);
+      setWidthtImg(img.clientWidth);
     }
   };
 
+  useEffect(() => {
+    const userAgent = navigator.userAgent.toLowerCase();
+    const isIPad = /ipad/.test(userAgent);
+    const isIPadPro = /macintosh/.test(userAgent) && "ontouchend" in document;
+    const isTablet =
+      /ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(
+        userAgent
+      );
+
+    const isMobile =
+      /iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm|ipad|tablet/i.test(
+        userAgent
+      );
+    if (isMobile || isIPad || isIPadPro || isTablet) {
+      setInstructionText("(click on the image)");
+    } else {
+      setInstructionText("(hover over the image)");
+    }
+  }, []);
+
   return (
     <div className="rootRotateImage">
+      <h2>Rotate Image</h2>
+      <p>{instructionText}</p>
       <div
         style={{
-          height: rotateImg == 180 || rotateImg == 360 ? `${400}px` : ``,
+          height: rotateImg == 180 || rotateImg == 360 ? `${widthtImg}px` : ``,
           width: rotateImg == 180 || rotateImg == 360 ? `${heightImg}px` : "",
         }}
         id="container"
