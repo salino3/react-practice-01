7fbd5f7b076294159abb74ee64037a7e7cf775f8
Salino3 <flaviobcn012@gmail.com>
2024-06-15 09:35:16 +0200

    add styles

--- src/pods/home/components/image-rotate/image-rotate.component.tsx
+++ src/pods/home/components/image-rotate/image-rotate.component.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./rotate-image.styles.scss";
 
 export const RotateImage: React.FC = () => {
+  const [rotateImg, setRotateImg] = useState(90);
+
+  const handleRotate = () => {
+    setRotateImg(rotateImg + 90);
+    const img: HTMLImageElement | null = document.querySelector("img");
+    if (img) {
+      img.style.transform = `rotate(${rotateImg}deg)`;
+    }
+  };
+
   return (
     <div className="rootRotateImage">
       <div className="container">
-        <i className="fas fa-sync-alt iconImg"></i>
-        <img src="/random-image-01.jpg" alt="Random Image" />
+        <div className="containerIcon">
+          <i onClick={handleRotate} className="fas fa-sync-alt iconImg"></i>
+        </div>
+        <img src="/random-image-02.jpg" alt="Random Image" />
       </div>
     </div>
   );
